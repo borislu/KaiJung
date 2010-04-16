c94f19bf5321dddcd7e6f2c3c6fcdb4492d5bf5c
Boris <delian@ether>
2010-04-16 09:28:45 +0800

    Fix 備貨單以條碼輸入
    Signed-off-by: Boris <delian@ether>

--- web/js/barcode.js
+++ web/js/barcode.js
@@ -1,6 +1,7 @@
 /**
  * boris.lds@gmail.com
- * Version : 1.00
+ * Version : 1.01 
+ * Update : 2010/4/16
  * By Boris Lu Taiwan
  * License : BSD
  */
@@ -14,42 +15,42 @@
                     }
                 }
             function domo(){ 
-                shortcut.add("1",function() { isBarcode( '1' ); }, {'propagate':true} ); 
-                shortcut.add("2",function() { isBarcode( '2' ); }, {'propagate':true} ); 
-                shortcut.add("3",function() { isBarcode( '3' ); }, {'propagate':true} ); 
-                shortcut.add("4",function() { isBarcode( '4' ); }, {'propagate':true} ); 
-                shortcut.add("5",function() { isBarcode( '5' ); }, {'propagate':true} ); 
-                shortcut.add("6",function() { isBarcode( '6' ); }, {'propagate':true} ); 
-                shortcut.add("7",function() { isBarcode( '7' ); }, {'propagate':true} ); 
-                shortcut.add("8",function() { isBarcode( '8' ); }, {'propagate':true} ); 
-                shortcut.add("9",function() { isBarcode( '9' ); }, {'propagate':true} ); 
-                shortcut.add("0",function() { isBarcode( '0' ); }, {'propagate':true} ); 
-                shortcut.add("a",function() { isBarcode( 'a' ); }, {'propagate':true} ); 
-                shortcut.add("b",function() { isBarcode( 'b' ); }, {'propagate':true} ); 
-                shortcut.add("c",function() { isBarcode( 'c' ); }, {'propagate':true} ); 
-                shortcut.add("d",function() { isBarcode( 'd' ); }, {'propagate':true} ); 
-                shortcut.add("e",function() { isBarcode( 'e' ); }, {'propagate':true} ); 
-                shortcut.add("f",function() { isBarcode( 'f' ); }, {'propagate':true} ); 
-                shortcut.add("g",function() { isBarcode( 'g' ); }, {'propagate':true} ); 
-                shortcut.add("h",function() { isBarcode( 'h' ); }, {'propagate':true} ); 
-                shortcut.add("i",function() { isBarcode( 'i' ); }, {'propagate':true} ); 
-                shortcut.add("j",function() { isBarcode( 'j' ); }, {'propagate':true} ); 
-                shortcut.add("k",function() { isBarcode( 'k' ); }, {'propagate':true} ); 
-                shortcut.add("l",function() { isBarcode( 'l' ); }, {'propagate':true} ); 
-                shortcut.add("m",function() { isBarcode( 'm' ); }, {'propagate':true} ); 
-                shortcut.add("n",function() { isBarcode( 'n' ); }, {'propagate':true} ); 
-                shortcut.add("o",function() { isBarcode( 'o' ); }, {'propagate':true} ); 
-                shortcut.add("p",function() { isBarcode( 'p' ); }, {'propagate':true} ); 
-                shortcut.add("q",function() { isBarcode( 'q' ); }, {'propagate':true} ); 
-                shortcut.add("r",function() { isBarcode( 'r' ); }, {'propagate':true} ); 
-                shortcut.add("s",function() { isBarcode( 's' ); }, {'propagate':true} ); 
-                shortcut.add("t",function() { isBarcode( 't' ); }, {'propagate':true} ); 
-                shortcut.add("u",function() { isBarcode( 'u' ); }, {'propagate':true} ); 
-                shortcut.add("v",function() { isBarcode( 'v' ); }, {'propagate':true} ); 
-                shortcut.add("w",function() { isBarcode( 'w' ); }, {'propagate':true} ); 
-                shortcut.add("x",function() { isBarcode( 'x' ); }, {'propagate':true} ); 
-                shortcut.add("y",function() { isBarcode( 'y' ); }, {'propagate':true} ); 
-                shortcut.add("z",function() { isBarcode( 'z' ); }, {'propagate':true} ); 
+                shortcut.add("1",function() { isBarcode( '1' ); }, {'disable_in_input':true } ); 
+                shortcut.add("2",function() { isBarcode( '2' ); }, {'disable_in_input':true } ); 
+                shortcut.add("3",function() { isBarcode( '3' ); }, {'disable_in_input':true } ); 
+                shortcut.add("4",function() { isBarcode( '4' ); }, {'disable_in_input':true } ); 
+                shortcut.add("5",function() { isBarcode( '5' ); }, {'disable_in_input':true } ); 
+                shortcut.add("6",function() { isBarcode( '6' ); }, {'disable_in_input':true } ); 
+                shortcut.add("7",function() { isBarcode( '7' ); }, {'disable_in_input':true } ); 
+                shortcut.add("8",function() { isBarcode( '8' ); }, {'disable_in_input':true } ); 
+                shortcut.add("9",function() { isBarcode( '9' ); }, {'disable_in_input':true } ); 
+                shortcut.add("0",function() { isBarcode( '0' ); }, {'disable_in_input':true } ); 
+                shortcut.add("a",function() { isBarcode( 'a' ); }, {'disable_in_input':true } ); 
+                shortcut.add("b",function() { isBarcode( 'b' ); }, {'disable_in_input':true } ); 
+                shortcut.add("c",function() { isBarcode( 'c' ); }, {'disable_in_input':true } ); 
+                shortcut.add("d",function() { isBarcode( 'd' ); }, {'disable_in_input':true } ); 
+                shortcut.add("e",function() { isBarcode( 'e' ); }, {'disable_in_input':true } ); 
+                shortcut.add("f",function() { isBarcode( 'f' ); }, {'disable_in_input':true } ); 
+                shortcut.add("g",function() { isBarcode( 'g' ); }, {'disable_in_input':true } ); 
+                shortcut.add("h",function() { isBarcode( 'h' ); }, {'disable_in_input':true } ); 
+                shortcut.add("i",function() { isBarcode( 'i' ); }, {'disable_in_input':true } ); 
+                shortcut.add("j",function() { isBarcode( 'j' ); }, {'disable_in_input':true } ); 
+                shortcut.add("k",function() { isBarcode( 'k' ); }, {'disable_in_input':true } ); 
+                shortcut.add("l",function() { isBarcode( 'l' ); }, {'disable_in_input':true } ); 
+                shortcut.add("m",function() { isBarcode( 'm' ); }, {'disable_in_input':true } ); 
+                shortcut.add("n",function() { isBarcode( 'n' ); }, {'disable_in_input':true } ); 
+                shortcut.add("o",function() { isBarcode( 'o' ); }, {'disable_in_input':true } ); 
+                shortcut.add("p",function() { isBarcode( 'p' ); }, {'disable_in_input':true } ); 
+                shortcut.add("q",function() { isBarcode( 'q' ); }, {'disable_in_input':true } ); 
+                shortcut.add("r",function() { isBarcode( 'r' ); }, {'disable_in_input':true } ); 
+                shortcut.add("s",function() { isBarcode( 's' ); }, {'disable_in_input':true } ); 
+                shortcut.add("t",function() { isBarcode( 't' ); }, {'disable_in_input':true } ); 
+                shortcut.add("u",function() { isBarcode( 'u' ); }, {'disable_in_input':true } ); 
+                shortcut.add("v",function() { isBarcode( 'v' ); }, {'disable_in_input':true } ); 
+                shortcut.add("w",function() { isBarcode( 'w' ); }, {'disable_in_input':true } ); 
+                shortcut.add("x",function() { isBarcode( 'x' ); }, {'disable_in_input':true } ); 
+                shortcut.add("y",function() { isBarcode( 'y' ); }, {'disable_in_input':true } ); 
+                shortcut.add("z",function() { isBarcode( 'z' ); }, {'disable_in_input':true } ); 
                 shortcut.add("Tab",function() { str=""; });//清除 str ，準備讀入新的條碼 
             }//domo
             jQuery(document).ready(domo);
